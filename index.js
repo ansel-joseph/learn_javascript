35fc5513c20dde77c10a7416ce6b1956c7f008fa
Ansel Joseph <anseljoseph2509@gmail.com>
2025-08-15 18:57:09 +0530

    add: switch statement

--- index.js
+++ index.js
@@ -1,4 +1,14 @@
-let age=20
-
-let result= age>18? "can vote": "cannot vote";
-console.log(result);
+let expr="Earth";
+switch(expr){
+    case "Mercury":
+        console.log("Mercury is the first planet")
+        break;
+    case "Venus":
+        console.log("Venus is the hottest planet")
+        break;
+    case "Earth":
+        console.log("Earth is the only planet to sustain life")
+        break;
+    case "Mars":
+        console.log("Mars was once a habitable planet")
+}
