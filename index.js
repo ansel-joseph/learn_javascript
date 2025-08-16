a761e174efbcc8f91f9bfe57b9cef482b8d88c33
Ansel Joseph <anseljoseph2509@gmail.com>
2025-08-16 19:21:03 +0530

    add: string_methods

--- index.js
+++ index.js
@@ -1,13 +1,4 @@
-
-let game_num = 23;
-
-for(i=0; i=10; i++){
-    user=prompt("Guess the number");
-    if(user==game_num){
-        console.log("You're guess is correct");
-        break;
-    }else{
-        console.log("You're guess is wrong");
-    }
-}
+username=prompt("Enter your name");
+usr="@"+username+username.length
+console.log(usr)
     
