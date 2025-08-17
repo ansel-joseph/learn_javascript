9dab532cc14943a11d3c1536c9a5c9908b02a21c
Ansel Joseph <anseljoseph2509@gmail.com>
2025-08-17 19:13:13 +0530

    add: avg_of_marks

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
-let countries= ["Russia", "USA", "Japan", "China", "India"]
-
-for(let country of countries){
-    console.log(country.toUpperCase());
+let marks = [85, 97, 44, 37, 76, 60];
+let sum = 0;
+for (i = 0; i < marks.length; i++) {
+    sum += marks[i];
 }
+average = sum / marks.length;
+console.log(average);
