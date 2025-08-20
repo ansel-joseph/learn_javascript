fb7cfa610041b03791bcd1ab5412352e474d54a9
Ansel Joseph <anseljoseph2509@gmail.com>
2025-08-20 19:29:38 +0530

    add : vowels_count

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
-//Arrow Functions
-const div = (a, b) => {
-    return a / b
+function countVowels(str){
+    count=0
+    for(const char of str.toLowerCase()){
+        if(char=="a"||char=="e"||char=="i"||char=="o"||char=="u"){
+            count++
+        }
+    }
+    console.log("The total vowels are: ", count)
 }
-let divi = div(10, 2)
-console.log(divi)
+
