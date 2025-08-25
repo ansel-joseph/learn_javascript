a235ada7d3ebdc81f257d59bce1a4ea5041e5f8e
Ansel Joseph <anseljoseph2509@gmail.com>
2025-08-25 19:35:01 +0530

    add: callback_methods

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
-let arr = ["Ansel", "Brandon", "Caleb", "Derrick", "Ethan"];
-arr.forEach((val, idx, arr) => {
-    console.log(val.toUpperCase(), idx, arr.length);
+let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+ //FILTER
+let even =arr.filter ((val) => {
+    return val % 2 === 0;
 });
+
+//Reduce
+console.log(even);
+let output = arr.reduce((acc, val) => {
+    return acc>val ? acc : val;
+});
+console.log(output);
