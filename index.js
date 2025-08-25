acaa7cce16a1d30dab272bf0889a295b31c72e69
Ansel Joseph <anseljoseph2509@gmail.com>
2025-08-25 19:15:50 +0530

    add: foreach

--- index.js
+++ index.js
@@ -1,10 +1,4 @@
-function countVowels(str){
-    count=0
-    for(const char of str.toLowerCase()){
-        if(char=="a"||char=="e"||char=="i"||char=="o"||char=="u"){
-            count++
-        }
-    }
-    console.log("The total vowels are: ", count)
-}
-
+let arr = ["Ansel", "Brandon", "Caleb", "Derrick", "Ethan"];
+arr.forEach((val, idx, arr) => {
+    console.log(val.toUpperCase(), idx, arr.length);
+});
