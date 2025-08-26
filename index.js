13d43c8ebec7a9388b3a8e652d34f8a1bdd96733
Ansel Joseph <anseljoseph2509@gmail.com>
2025-08-26 22:49:07 +0530

    add: css_added and alert_used

--- index.js
+++ index.js
@@ -1,13 +1,1 @@
-let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-
- //FILTER
-let even =arr.filter ((val) => {
-    return val % 2 === 0;
-});
-
-//Reduce
-console.log(even);
-let output = arr.reduce((acc, val) => {
-    return acc>val ? acc : val;
-});
-console.log(output);
+alert("Hello The Guy In Christ")
